fe2fa8461d3186c3d0475be57f8d47a7e95278ea
Toustsik Mikahil <toustsikmikhail@gmail.com>
2024-12-10 19:14:48 +0100

    feat: add main layout component

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./layout/header/header.component";
+import { LayoutComponent } from "./layout/layout.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  // imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [LayoutComponent]
 })
 export class AppComponent {
-  title = 'app';
+  title = 'elsebet';
 }
--- app/src/app/layout/layout.component.ts
+++ app/src/app/layout/layout.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+import { MainComponent } from "./main/main.component";
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [HeaderComponent, FooterComponent, MainComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
