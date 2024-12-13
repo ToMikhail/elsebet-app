df8468cb0d953304f47d4da3ce20853ba2edbb81
Toustsik Mikahil <toustsikmikhail@gmail.com>
2024-12-13 16:06:21 +0100

    feat: add some components

--- app/src/app/layout/header/header.component.ts
+++ app/src/app/layout/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { LogoComponent } from "../../shared/logo/logo.component";
+import { NavbarComponent } from "../../shared/navbar/navbar.component";
 
 @Component({
   selector: 'app-header',
-  imports: [LogoComponent],
+  imports: [LogoComponent, NavbarComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
