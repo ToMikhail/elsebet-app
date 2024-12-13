4283118721e2fe0c9b5a39ced90f9998a797e3e0
Toustsik Mikahil <toustsikmikhail@gmail.com>
2024-12-13 18:22:39 +0100

    feat: add nocigation

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { FrontPageComponent } from './layout/main/components/front-page/front-page.component';
+import { NewsComponent } from './layout/main/components/news/news.component';
+import { AboutUsComponent } from './layout/main/components/about-us/about-us.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: FrontPageComponent},
+  { path: 'news', component: NewsComponent },
+  { path: 'about-us', component: AboutUsComponent },
+];
